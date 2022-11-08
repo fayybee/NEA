d20b9f633b2f49180ca47a25416281130a4cf506
feibi <feibi.allen@gmail.com>
2022-11-08 15:27:50 +0000

    Fixed adjacency list

--- BackEndCalc.py
+++ BackEndCalc.py
@@ -1,30 +1,45 @@
 def makeGraphAdjacencyList(CircuitGrid):
-    firstComponantCoord = findFirstComponentCoords(CircuitGrid) #change this so that it is a list of coords of all nodes instead of just the coords of the first
+    componantCoords = findComponentCoords(CircuitGrid)
     adjacencyList = {}
-    try:
-        firstComponantRow = firstComponantCoord[0]
-        firstComponantCol = firstComponantCoord[1]
-        adjacencys = checkForAdjacencies(CircuitGrid,firstComponantRow,firstComponantCol)
-    except:
-        print("no components")
+    for i in range(0, len(componantCoords)):
+        row = str(componantCoords[i][0])
+        col = str(componantCoords[i][1])
+        adjacencyList[row + col] = checkForAdjacencies(CircuitGrid, int(row), int(col))
+    print(adjacencyList)
 
-# change this so that it find the location of all components and returns a list of coordinates of all the components,
-# use this information to make the adjacency list so that you make sure none are missing
-def findFirstComponentCoords(array):
+
+def findComponentCoords(array):
+    coordsList = []
     for row in range(len(array)):
         for column in range(len(array[row])):
             if array[row][column] != "":
-                return row,column
+                coordsList.append([row, column])
+    return coordsList
 
 
-def checkForAdjacencies(array,row,col):
-    pass
+def checkForAdjacencies(array, row, col):
+    adjacentCoordsList = []
+    # checks positions around the coordinate as long as there are not going to be off  the grid
+    if row > 0:
+        if array[row - 1][col] != "":
+            adjacentCoordsList.append([row - 1, col])
+    if col > 0:
+        if array[row][col - 1] != "":
+            adjacentCoordsList.append([row, col - 1])
+    if row < (len(array) - 1):
+        if array[row + 1][col] != "":
+            adjacentCoordsList.append([row + 1, col])
+    if col < (len(array[0])):
+        if array[row][col + 1] != "":
+            adjacentCoordsList.append([row, col + 1])
+    return adjacentCoordsList
 
-array= [
-    ["","","","",""],
-    ["","","","",""],
-    ["","","","",""],
-    ["","","","",""]
+
+array = [
+    ["", "", "", "q", ""],
+    ["", "", "", "", ""],
+    ["", "", "", "", ""],
+    ["", "", "", "", ""]
 ]
 
 makeGraphAdjacencyList(array)
